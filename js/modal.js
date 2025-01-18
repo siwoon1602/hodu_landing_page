b1b727a72eadbf064405e9070685a2f0d355d223
siwoon1602 <siwoon1602@gmail.com>
2025-01-18 14:24:58 +0900

    subscrive 벨리데이션 , 오류 콘솔 추가

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 const modal = document.querySelector(".subsrive_modal_button");
-const onModal = document.querySelector("dialog");
+const onModal = document.querySelector(".modal");
 const subsriveText = document.querySelector(".subsribe_email");
 const offModal = document.querySelector(".modal_off");
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -7,9 +7,11 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 modal.addEventListener("click", () => {
   const inputValue = subsriveText.value.trim();
   if (!inputValue || !emailRegex.test(inputValue)) {
+    console.log("올바른 이메일 형식으로 입력해주세요");
     return;
   }
-  modal.showModal();
+  onModal.showModal();
+  subsriveText.value = "";
 });
 
 offModal.addEventListener("click", () => {
